ef7cc032e09caebc066f3a3b5c28b190394168ad
Urva-Patel28 <patelurva2806@gmail.com>
2024-12-04 14:40:59 +0530

    - Add comments in Creon job service

--- src/cleanup.service.ts
+++ src/cleanup.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource, LessThan } from 'typeorm';
 import { AwsConsoleCredentials } from 'src/entities/aws-console-credentials.entity';
 import { UserService } from './modules/user/user.service';
 
+/**
+ * It is responsible for periodically cleaning up expired AWS Console credentials.
+ */
 @Injectable()
 export class CleanupService {
   constructor(
@@ -12,10 +15,11 @@ export class CleanupService {
     @InjectDataSource()
     private readonly dataSource: DataSource,
   ) {}
-  @Cron('0 */2 * * * *') // Executes once at the start of every 2 minutes
+  @Cron(CronExpression.EVERY_DAY_AT_10AM) // Executes Every day at 10 AM
   async deleteExpiredCredentials() {
     const currentTime = new Date();
     try {
+      // Finds all AWS Console credentials with an expiration time less than the current time.
       const awsConsoleCredentials = await this.dataSource.manager.find(
         AwsConsoleCredentials,
         { where: { expiration_time: LessThan(currentTime) } },
@@ -23,10 +27,11 @@ export class CleanupService {
 
       for (const record of awsConsoleCredentials) {
         await this.userService.deleteAwsConsoleCredentials({
-          aws_username: record.aws_username,
+          aws_username: record.aws_username, // Deletes credentials associated with the expired AWS username.
         });
       }
     } catch (error) {
+      // Catches and rethrows any errors that occur during the process.
       throw error;
     }
   }
